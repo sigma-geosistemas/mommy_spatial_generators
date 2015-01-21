c923b1fe03e4322e8203fe8d021691bbc6836bf7
Matt Johnson <mdj2@pdx.edu>
2015-01-21 09:34:13 -0800

    Need to read in the contents of the README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
-	README = readme
+	README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
